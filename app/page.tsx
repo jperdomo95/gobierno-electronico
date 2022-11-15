60367f3a9d45f933cb8db938aadcfc26691c5c6a
julio.perdomo <jperdomocorrea@gmail.com>
2022-11-15 10:59:25 -0400

    Making a formal cover for final delivery

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,33 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Las TIC
-        </h1>
-        <h2 className={styles.textcenter}>en el Gobierno Electrónico</h2>
+        <header>
+          <p className={styles.header}>
+            REPÚBLICA BOLIVARIANA DE VENEZUELA <br/>
+            MINISTERIO DEL PODER POPULAR DE ECONOMÍA FINANZA Y COMERCIO EXTERIOR<br/>
+            ESCUELA NACIONAL DE ADMINISTRACIÓN Y HACIENDA PÚBLICA<br/>
+            DIRECCIÓN DE POSTGRADO Y ADIESTRAMIENTO<br/>
+            ESPECIALIZACIÓN EN CONTROL DE LA GESTIÓN PÚBLICA<br/>
+            GOBIERNO ELECTRÓNICO SECCIÓN 83
+          </p>
+        </header>
+        <h4>
+          LAS TIC EN EL GOBIERNO ELECTRÓNICO
+        </h4>
 
-        <p className={styles.description}>
+        {/* <p className={styles.description}>
           Las TIC no son buenas o malas en sí mismas, sino que son los usuarios quienes pueden hacer un uso bueno o malo de estas.
-        </p>
+        </p> */}
+
+        <div className={styles.credits}>
+          <div>Facilitador: Adrián Castro</div>
+          <div>
+            <p>Participante:</p>
+            <p>Xavier Mora <br />C.I 22904725</p>
+          </div>
+        </div>
+
+        <p>Caracas, Noviembre 2022</p>
 
         <div className={styles.grid}>
           <Link href="/introduccion" className={styles.card}>
